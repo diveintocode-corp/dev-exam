62026cc9f26baa68b166696c38edfd0bc6ea41aa
MachikoFujii <machiko.fujii@gmail.com>
2021-05-12 12:03:12 +0900

    Created Properties controller

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,25 @@
+class PropertiesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def confirm
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'properties/index'
+  get 'properties/new'
+  get 'properties/confirm'
+  get 'properties/create'
+  get 'properties/edit'
+  get 'properties/show'
+  get 'properties/update'
+  get 'properties/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
