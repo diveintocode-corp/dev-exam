78b41b627d9a29951b9d94d01fc441496159e3a1
MachikoFujii <machiko.fujii@gmail.com>
2021-05-12 12:09:50 +0900

    Created routes for properties controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'properties/index'
-  get 'properties/new'
-  get 'properties/confirm'
-  get 'properties/create'
-  get 'properties/edit'
-  get 'properties/show'
-  get 'properties/update'
-  get 'properties/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :properties
 end
